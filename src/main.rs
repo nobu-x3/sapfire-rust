2e2b0686bfae1454248e6720cdf9308ba4ce35b8
Dominik Kurasbediani <dominik_k@cherrypickgames.com>
2023-06-11 02:30:55 +0200

    Scaffolded renderer state's structure

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,38 @@
 use winit::{
     event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
-    window::WindowBuilder,
+    window::{Window, WindowBuilder},
 };
+
+struct RendererState {
+    surface: wgpu::Surface,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    config: wgpu::SurfaceConfiguration,
+    window: Window,
+}
+
+impl RendererState {
+    async fn new(window: Window) -> Self {
+        !todo!()
+    }
+
+    fn resize(&mut self, size: winit::dpi::PhysicalSize<u32>) {
+        todo!()
+    }
+
+    fn input(&mut self, event: &WindowEvent) {
+        !todo!()
+    }
+
+    fn update(&mut self) {
+        todo!()
+    }
+
+    fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
+        !todo!()
+    }
+}
 
 fn main() {
     env_logger::init();
